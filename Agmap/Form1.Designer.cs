573568e272a91d8461ce6611da161b334bb42f13
mikhp <mikhp@qq.com>
2016-01-14 14:46:17 +0800

    使用vs2010和github结合来进行代码控制！

---
+++ Agmap/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Agmap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(934, 489);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.OnPolygonEnter += new GMap.NET.WindowsForms.PolygonEnter(this.gMapControl1_OnPolygonEnter);
+            this.gMapControl1.OnPolygonLeave += new GMap.NET.WindowsForms.PolygonLeave(this.gMapControl1_OnPolygonLeave);
+            this.gMapControl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseClick);
+            this.gMapControl1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseDoubleClick);
+            this.gMapControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(812, 543);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "关闭";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 545);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(630, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 591);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Agmap/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace Agmap
+{
+    public partial class Form1 : Form
+    {
+        public GMapOverlay routes;
+        public GMapOverlay MyMark;
+        public GMapPolygon polygon;
+        public GMapMarker MyShop;
+        public GMapMarker Center;
+        public PointLatLng lastPosition;
+        public GeocodingProvider gp;
+        private GMapOverlay polygons = new GMapOverlay("polygon"); //放置polygon的图层
+        private GMapPolygon drawingPolygon = null; //正在画的polygon
+        private List<PointLatLng> drawingPoints = new List<PointLatLng>(); //多边形的点集
+        private GMapPolygon currentPolygon;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+
+            
+            this.gMapControl1.BackColor = Color.Red;
+            //设置控件的管理模式
+            this.gMapControl1.Manager.Mode = AccessMode.ServerAndCache;
+            //设置控件显示的地图来源
+            this.gMapControl1.MapProvider = GMapProviders.GoogleChinaMap;
+            //设置控件显示的当前中心位置
+
+            gp = GMapProviders.OpenStreetMap as GeocodingProvider;
+
+            //31.7543, 121.6281
+            this.gMapControl1.Position = new PointLatLng(28.210422, 112.976478);
+            //设置控件最大的缩放比例
+            this.gMapControl1.MaxZoom = 18;
+            //设置控件最小的缩放比例
+            this.gMapControl1.MinZoom = 1;
+            //设置控件当前的缩放比例
+            this.gMapControl1.Zoom = 11;
+            //创建一个新图层
+            routes = new GMapOverlay("routes");
+            MyMark = new GMapOverlay("MyMark");
+            routes.IsVisibile = true;//可以显示
+            MyMark.IsVisibile = true;
+            this.gMapControl1.Overlays.Add(routes);//添加到图层列表中
+            this.gMapControl1.Overlays.Add(MyMark);
+            //创建一个图标
+            GMapMarker gMapMarker = new GMarkerGoogle(gMapControl1.Position, GMarkerGoogleType.arrow);
+            //添加图层routes中
+            gMapMarker.ToolTipText = "我的商店";
+            this.MyMark.Markers.Add(gMapMarker);
+            // this.gMapControl1.Dock = DockStyle.Fill;//将控件全屏显示
+            gMapControl1.Overlays.Add(polygons);
+        }
+
+        private void gMapControl1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            polygons.Polygons.Add(drawingPolygon);
+            drawingPolygon = null;
+            drawingPoints.Clear();
+        }
+
+        private void gMapControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            PointLatLng point = gMapControl1.FromLocalToLatLng(e.X, e.Y);
+            textBox1.Text = string.Format("x:{0}  y:{1}", point.Lat, point.Lng);
+        }
+
+
+        private void gMapControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                PointLatLng point = gMapControl1.FromLocalToLatLng(e.X, e.Y);
+                Bitmap bitmap = Bitmap.FromFile("F:\\晴.png") as Bitmap;
+                GMapMarker gmm = new GMapMarkerImage(point, bitmap);
+                gmm.ToolTipText = string.Format("x:{0}  y:{1}", point.Lat, point.Lng);
+                gmm.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+                this.MyMark.Markers.Add(gmm);
+                //ddd
+                drawingPoints.Add(point);
+                if (drawingPolygon == null)
+                {
+                    drawingPolygon = new GMapPolygon(drawingPoints, "my polygon");
+                    drawingPolygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
+                    drawingPolygon.Stroke = new Pen(Color.Blue, 1);
+                    drawingPolygon.IsHitTestVisible = true;
+                    polygons.Polygons.Add(drawingPolygon);
+                }
+                else
+                {
+                    drawingPolygon.Points.Clear();
+                    drawingPolygon.Points.AddRange(drawingPoints);
+                    if (polygons.Polygons.Count == 0)
+                    {
+                        polygons.Polygons.Add(drawingPolygon);
+                    }
+                    else
+                    {
+                        gMapControl1.UpdatePolygonLocalPosition(drawingPolygon);
+                    }
+                }
+            
+            }          
+
+        }
+
+        private void gMapControl1_OnPolygonEnter(GMapPolygon item)
+        {
+            currentPolygon = item;
+            item.Stroke.Color = Color.Red;
+        }
+
+        private void gMapControl1_OnPolygonLeave(GMapPolygon item)
+        {
+            currentPolygon = null;
+            item.Stroke.Color = Color.MidnightBlue;
+
+        }
+
+    }
+}
